a268ec040f1b071985fa0ec1e99d479a0281815a
NoMoreZer0 <kazakupersuper@mail.ru>
2023-08-08 12:53:00 +0600

    removed solution

--- Level 2/sum-1-n/src/main/java/com/kz/sum1n/Sum1ToN.java
+++ Level 2/sum-1-n/src/main/java/com/kz/sum1n/Sum1ToN.java
@@ -3,7 +3,6 @@ package com.kz.sum1n;
 public class Sum1ToN {
     public static int sum1n(int n) {
         // Write your code here
-        if (n % 2 == 0) return (n / 2) * (n + 1);
-        return n * ((n + 1) / 2);
+        return 0;
     }
 }
