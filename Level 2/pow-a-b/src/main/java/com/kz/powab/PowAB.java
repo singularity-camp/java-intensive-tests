d18e5a4435354abf86abb7b1893adb5f3d5b4c90
NoMoreZer0 <kazakupersuper@mail.ru>
2023-08-09 14:44:01 +0600

    readme upd + removed sol

--- Level 2/pow-a-b/src/main/java/com/kz/powab/PowAB.java
+++ Level 2/pow-a-b/src/main/java/com/kz/powab/PowAB.java
@@ -2,8 +2,6 @@ package com.kz.powab;
 
 public class PowAB {
     public static int pow(int a, int b) {
-        int res = 1;
-        for (int i = 1; i <= b; ++i) res *= a;
-        return res;
+        // Write your code here
     }
 }
