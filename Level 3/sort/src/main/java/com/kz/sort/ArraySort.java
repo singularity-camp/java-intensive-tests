8fe481ca808ff33d92f1fa9300bcf8067affd165
NoMoreZer0 <kazakupersuper@mail.ru>
2023-08-24 11:39:46 +0600

    removed solution

--- Level 3/sort/src/main/java/com/kz/sort/ArraySort.java
+++ Level 3/sort/src/main/java/com/kz/sort/ArraySort.java
@@ -2,15 +2,6 @@ package com.kz.sort;
 
 public class ArraySort {
     public static void sort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n - 1; ++j) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+        // Write your code here
     }
 }
